ebddb14e1e3f8942a5904621262cd30c882eec8f
Achyuth Varma <achyuth.varma1@gmail.com>
2021-03-23 11:18:16 +0530

    Created a new route

---
+++ src/routes/__test__/get-ticket.test.ts
@@ -0,0 +1,37 @@
+import request from "supertest";
+import { app } from "../../app";
+import jwt from "jsonwebtoken";
+import { Ticket } from "../../models/ticket";
+
+// test("Returns a 404 if ticket is not found", async () => {
+//   await request(app).get("/api/tickets/sahefhoohsaauciaghewua").expect(404);
+// });
+
+test("Returns the ticket if the ticket is found", async () => {
+  const payload = {
+    id: "asaeoih",
+    email: "test@test.com",
+  };
+
+  const token = jwt.sign(payload, "asdf");
+
+  const createdCookie = `user=${token}`;
+
+  const response = await request(app)
+    .post("/api/ticket")
+    .set("Cookie", createdCookie)
+    .send({ title: "Some Custom Title", price: 20.0 })
+    .expect(201);
+
+  // const ticket = Ticket.find({ id: response.body.ticket.id });
+
+  // console.log(ticket);
+
+  //   const ticketResponse = await request(app)
+  //     .get(`/api/tickets/${response.body.ticket.id}`)
+  //     .send()
+  //     .expect(200);
+
+  //   expect(ticketResponse.body.title).toEqual("Some Custom Title");
+  //   expect(ticketResponse.body.price).toEqual(20.0);
+});
